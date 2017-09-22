6b97104a47a382734152b55a05a130f13907c009
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2017-09-22 14:31:40 -0400

    Adds docstrings to methods.

--- newsdata_report.py
+++ newsdata_report.py
@@ -27,36 +27,30 @@ DAYS_WITH_ERROR_QRY = '''SELECT log_time, error_percent
 
 
 def connect():
-    """
-    Returns a connection object to the database.
-    """
+    """:returns : Connection object to the database specified in DBNAME"""
     return psycopg2.connect(database=DBNAME)
 
 
 def get_days_with_errors():
-    '''
-    Returns days when request had more than 1% of errors.
-    '''
+    """":returns : list of tuples of dates and error percentage."""
     return get_qry_result(DAYS_WITH_ERROR_QRY)
 
 
 def get_popular_authors():
-    '''
-    Return a list of popular authors.
-    '''
+    """:returns : list of tuples of author names and total views."""
     return get_qry_result(FAVORITE_AUTHORS_QRY)
 
 
 def get_most_popular_articles():
-    """
-    Returns a list of the three most popular articles of all time.
-    """
+    """:returns list: list of tuples with three most favorite articles."""
     return get_qry_result(FAVORITE_ARTICLES_QRY)
 
 
 def get_qry_result(qry):
     """
-    Returns a list of results based on the query string passed.
+    :param qry: string of the sql query to be executed
+
+    :returns tuple_list: list of tuples with the result of the query specified by the string qry 
     """
     conn = connect()
     cur = conn.cursor()
@@ -67,8 +61,7 @@ def get_qry_result(qry):
 
 
 def print_days_with_errors():
-    '''
-    '''
+    """Prints table with date (YYYY-MM-DD) and error percentage, if error percentage is greater than 1%."""
     days_with_errors_list = get_days_with_errors()
     print(" -" * 12)
     print("|{:^12}|{:^8} |".format("Date", "Error %"))
@@ -78,12 +71,16 @@ def print_days_with_errors():
     print(" -" * 12)
 
 
-def print_report(result_list, header_tupple):
-    '''
-    print method for a list that contains query results.
-    '''
+def print_report(result_list, header_tuple):
+    """
+    Prints a report using a list and column headers from a tuple.
+
+    :param result_list: list of tuples from a query result
+
+    :param header_tuple: Tuples with the column headers
+    """
     print(" -" * 22)
-    print("|{:^32}|{:^8} |".format(header_tupple[0], header_tupple[1]))
+    print("|{:^32}|{:^8} |".format(header_tuple[0], header_tuple[1]))
     print(" -" * 22)
     for item in result_list:
         print("|{:<32}|{:^8} |".format(item[0], item[1]))
@@ -91,17 +88,13 @@ def print_report(result_list, header_tupple):
 
 
 def print_popular_articles():
-    '''
-    Tabulated print out of three most popular articles.
-    '''
+    """Tabulated print out of three most popular articles."""
     popular_articles_list = get_most_popular_articles()
     print_report(popular_articles_list, ("Article title", "Views"))
 
 
 def print_popular_authors():
-    '''
-    Tabulated print out popular authors.
-    '''
+    """Tabulated print out popular authors."""
     popular_authors_list = get_popular_authors()
     print_report(popular_authors_list, ("Authors Name", "Views"))
 
